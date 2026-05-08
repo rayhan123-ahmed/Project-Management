a7ce5432edc2db47fbe4ad6269879ed9a0e1fbbc
Rayhan Ahmed <rmd09181@gmail.com>
2026-05-08 20:54:15 +0600

    12. Express configuration and CORS

--- src/app.js
+++ src/app.js
@@ -1,6 +1,27 @@
 import express from "express";
+import cors from 'cors'
 
 const app = express();
+
+// basic configurations
+
+// Reads JSON body
+app.use(express.json({limit:'16kb'}));
+// Reads form data
+app.use(express.urlencoded({extended:true, limit:'16kb'}));
+// Serves static files like images/CSS/JS
+app.use(express.static('public'));
+
+
+// cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
 
 app.get("/instagram", (req, res) => {
   res.send("You opened a instagram account");
