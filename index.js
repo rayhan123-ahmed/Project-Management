4fb5158a18761b172f5513c03591bd74421da617
Rayhan Ahmed <rmd09181@gmail.com>
2026-05-08 14:31:50 +0600

    add dotenv file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: "./.env",
+});
+
+let MyUsername = process.env.userName;
+let password = process.env.password;
+
+console.log(`value`, password);
+console.log(`value`, MyUsername);
+
 console.log("start the backend system");
