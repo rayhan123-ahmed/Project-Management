89924c68c2af194ab8df1fb1c42e01842579c34d
Rayhan Ahmed <rmd09181@gmail.com>
2026-05-23 23:27:54 +0600

    genrate access token and refresh token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new mongoose.Schema(
   {
@@ -64,14 +65,40 @@ const userSchema = new mongoose.Schema(
 );
 
 // pre hoocks for hasing password
-userSchema.pre('save', async function (next) {
-    if(!this.isModified('password')) return next()
-    this.password = await bcrypt.hash(this.password,10);
-    next()
-})
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
 //  methods for comapred password
-userSchema.methods.isPasswordCorrect = async function(password) {
-     return await bcrypt.compare(password,this.password)
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+// genrate access token
+userSchema.methods.genrateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      userName: this.userName,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCES_TOKEN_EXPIRY },
+  );
+};
+
+// genrate refresh token
+userSchema.methods.genrateRefreshToken = function () {
+   return jwt.sign(
+     {
+       _id: this._id,
+     },
+     process.env.REFRESH_TOKEN_SECRET,
+     { expiresIn: process.env.REFRESH_TOKEN_EXPIRY },
+   );
 }
+
 
 export default user;
